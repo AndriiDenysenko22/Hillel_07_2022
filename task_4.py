71edbb436716e9012b97e21e3f64bafc2edc486a
AndriiDenysenko22 <St4r1r@gmail.com>
2022-08-08 18:00:34 +0300

    Завдання 4

--- task_4.py
+++ task_4.py
@@ -1,6 +1,6 @@
-#Код буде відкидати спеціальні символи, розділові знаки та цифри автоматично
+# Код буде відкидати спеціальні символи, розділові знаки та цифри автоматично
 a = input('Введіть речення, яке складається з двох слів (бажано без розділових знаків цифр та спеціальних символів): ')
-
+b = a
 symbols = '''!()-[]{};:/@#$%^'"\,.&*_~1234567890+='''
 
 for i in a:
@@ -10,18 +10,22 @@ for i in a:
 length = len(a.split())
 
 if length == 2:
+    # дві нові змінні:
     var = a.split()[0]
     var1 = a.split()[1]
-    # змінюємо порядок слів:
+    # змінюємо порядок слів і змінюємо регістри літер:
     new_var1 = var1.upper()
     new_var = var.lower().title()
-    new_string_1 = f'!{new_var1} {new_var}?'
-    new_string_2 = '!{} {}?'.format(new_var1, new_var)
-    new_string_3 = '!{0} {1}?'.format(new_var1, new_var)
+    # В завданні не вказано щодо розділових знаків перед/після ! ?,
+    # тому тут після знаку оклику пробіл, а перед знаком питання пробіл не ставиться
+    # (я взяв за основу друк в Word)
+    new_string_1 = f'! {new_var1} {new_var}?'
+    new_string_2 = '! {} {}?'.format(new_var1, new_var)
+    new_string_3 = '! {0} {1}?'.format(new_var1, new_var)
 
     my_file = open('py.txt', 'w')
 
-    print(a, new_var, new_var1, new_string_1, new_string_2, new_string_3, sep="\n <<<>>> \n", file=my_file)
+    print(b, var, var1, new_string_1, new_string_2, new_string_3, sep="\n <<<>>> \n", file=my_file)
     my_file.close()
 
 elif length < 2:
