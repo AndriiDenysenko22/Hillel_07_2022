8d1275e8b6f430e5a28e6e91651b21d851535995
AndriiDenysenko22 <St4r1r@gmail.com>
2022-08-14 17:03:42 +0300

    ДЗ 9. Работа со словарями

---
+++ Task_9.py
@@ -0,0 +1,6 @@
+#Це все, чи потрібно було ще щось доробити?
+old_dict = {'asd': 321, 'hoho': 3192, 'hist': 42}
+new_dict = {value: key for key, value in old_dict.items()}
+
+print(old_dict)
+print(new_dict)
