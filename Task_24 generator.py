aa07d7860653cd2f2d13a49819ccfab272612717
AndriiDenysenko22 <st4r1r@gmail.com>
2022-09-18 13:46:18 +0300

    Task_24: generators of progression

--- Task_24 generator.py
+++ Task_24 generator.py
@@ -1,7 +1,7 @@
 # Это умножение на какое-то заданное число, к примеру, геометрическая прогрессия на 3 от единицы:
 # 1, 3, 9, 27, 81... и т.д.
 
-def geo_progres(i):
+def geo_progression(i):
     for value in range(i):
         yield 3**value
         value += 1
